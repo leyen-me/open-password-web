ae16e779b5d66824f97879953baec1d2ac6e2c97
DIFFFFFFT <672228275@qq.com>
2024-03-20 19:08:07 +0800

    优化细节

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+    <div class="border-b border-gray-200 bg-white px-8 py-5 sm:px-8">
         <div class="-ml-4 -mt-4 flex flex-wrap items-center justify-between sm:flex-nowrap">
             <div class="ml-4 mt-4">
                 <h3 class="text-base font-semibold leading-6 text-gray-900">OPENPASSWORD</h3>
